0afaa6991df0dd7dbafdd89cb6dd82c4343e77d5
Duy <truongvanduy09072002@gmail.com>
2024-03-22 19:27:02 +0700

    feat: add foreign key constraints

--- backend/src/models/student_class.model.js
+++ backend/src/models/student_class.model.js
@@ -4,53 +4,49 @@ const Class = require('./class.model');
 const Student = require('./student.model');
 
 const StudentClass = sequelize.define('student_class', {
-  // gradeId: {
-  //   type: DataTypes.INTEGER,
-  //   allowNull: false,
-  //   primaryKey: true,
-  // },
-  // yearId: {
-  //   type: DataTypes.INTEGER,
-  //   allowNull: false,
-  //   primaryKey: true,
-  // },
-  // classOrder: {
-  //   type: DataTypes.INTEGER,
-  //   allowNull: false,
-  //   primaryKey: true,
-  // },
-  // studentId: {
-  //   type: DataTypes.INTEGER,
-  //   allowNull: false,
-  //   primaryKey: true,
-  // },
+  gradeId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+  yearId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+  classOrder: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+  studentId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
 });
 
-Class.belongsToMany(Student, {
-  foreignKey: 'classOrder',
-  through: StudentClass,
-});
-Student.belongsToMany(Class, {
-  foreignKey: 'studentId',
-  through: StudentClass,
-});
-StudentClass.belongsTo(Class, { foreignKey: 'yearId', constraints: true });
-Class.hasMany(StudentClass, { foreignKey: 'yearId' });
-StudentClass.belongsTo(Class, { foreignKey: 'gradeId', constraints: true });
-Class.hasMany(StudentClass, { foreignKey: 'gradeId' });
-StudentClass.belongsTo(Student);
-Student.hasMany(StudentClass);
+// const queryInterface = sequelize.getQueryInterface();
 
-const queryInterface = sequelize.getQueryInterface();
-queryInterface.addConstraint('student_class', {
-  fields: ['gradeId', 'yearId', 'classOrder', 'studentId'],
-  type: 'primary key',
-});
-queryInterface.addConstraint('student_class', {
-  fields: ['gradeId', 'yearId'],
-  type: 'foreign key',
-});
+// StudentClass.sync().then(() => {
+//   queryInterface.addConstraint('student_class', {
+//     fields: ['grade_id', 'year_id', 'class_order'],
+//     type: 'foreign key',
+//     references: {
+//       table: 'class',
+//       fields: ['grade_id', 'year_id', 'order'],
+//     },
+//   });
+//   queryInterface.addConstraint('student_class', {
+//     fields: ['student_id'],
+//     type: 'foreign key',
+//     references: {
+//       table: 'student',
+//       fields: ['id'],
+//     },
+//   });
+// });
 
-StudentClass.sync({ force: true });
+StudentClass.sync();
 
 module.exports = StudentClass;
