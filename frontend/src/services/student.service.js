da1003ef7cbf9fdd0cf76ae9c9806f842e412559
Duy <truongvanduy09072002@gmail.com>
2024-04-28 14:32:01 +0700

    refactor: group 'student' into baseURL

--- frontend/src/services/student.service.js
+++ frontend/src/services/student.service.js
@@ -2,27 +2,43 @@ import createApiClient from './api.service'
 
 class StudentService {
   constructor() {
-    this.api = createApiClient('/api')
+    this.api = createApiClient('/api/student')
+  }
+
+  async getAll() {
+    return (await this.api.get(`/`)).data
   }
 
   async getByEmail(email) {
-    return await this.api.get(`/student?email=${email}`)
+    return await this.api.get(`?email=${email}`)
   }
 
   async getProfile(id) {
-    return (await this.api.get(`/student/profile/${id}`)).data
+    return (await this.api.get(`/profile/${id}`)).data
   }
 
   async getScores(filter) {
-    return (await this.api.get(`/student/score`, { params: filter })).data
+    return (await this.api.get(`/score`, { params: filter })).data
   }
 
-  async login(data) {
-    return await this.api.post(`/student/login`, data)
+  async create() {
+    return (await this.api.get(`/create`)).data
   }
 
-  async logout() {
-    return await this.api.post(`/student/logout`)
+  async store(data) {
+    return (await this.api.post(`/create`, data)).data
+  }
+
+  async edit(id) {
+    return (await this.api.get(`/edit/${id}`)).data
+  }
+
+  async update(id, data) {
+    return (await this.api.patch(`/edit/${id}`, data)).data
+  }
+
+  async delete(id) {
+    return (await this.api.delete(`/delete/${id}`)).data
   }
 }
 export default new StudentService()
