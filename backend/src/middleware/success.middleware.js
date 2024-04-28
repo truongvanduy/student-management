c2e75e487a31ef8aa555aefff52107048fc9c3a9
Duy <truongvanduy09072002@gmail.com>
2024-04-28 20:03:11 +0700

    feat: user higher order func to pass optional message

--- backend/src/middleware/success.middleware.js
+++ backend/src/middleware/success.middleware.js
@@ -1,5 +1,7 @@
-module.exports = async function (req, res, next) {
-  return res.status(200).json({
-    status: 'ok',
-  });
-};
+module.exports = (message) =>
+  async function (req, res, next) {
+    return res.status(200).json({
+      status: 'ok',
+      message: message || 'Thành công',
+    });
+  };
