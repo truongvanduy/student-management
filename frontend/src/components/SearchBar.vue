0d6625b49cbc7ee71898041a445d9633a16be0fd
Duy <truongvanduy09072002@gmail.com>
2024-04-28 14:29:49 +0700

    feat: implement new components

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import MdIcon from '@/components/icons/MdIcon.vue'
+defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  },
+  placeholder: {
+    type: String,
+    default: 'Tìm kiếm'
+  }
+})
+const emit = defineEmits(['submit', 'update:modelValue'])
+
+function updateModelValue(e) {
+  emit('update:modelValue', e.target.value)
+}
+
+function submit() {
+  emit('submit')
+}
+</script>
+<template>
+  <form
+    action="#"
+    method="get"
+    class="search"
+  >
+    <md-outlined-text-field
+      type="search"
+      name="q"
+      :value="modelValue"
+      @input="updateModelValue"
+      @keyup.enter="submit"
+      :placeholder="placeholder"
+    >
+      <MdIcon slot="leading-icon">search</MdIcon>
+    </md-outlined-text-field>
+  </form>
+</template>
+
+<style lang="scss" src="../assets/scss/components/_search.scss"></style>
